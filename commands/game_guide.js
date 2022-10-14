8894296daf025503c9600a1e81053a0ea71d7d32
Dainel Vera <arubinofaux@gmail.com>
2022-10-14 02:34:04 +0000

    /game-guide command

--- commands/game_guide.js
+++ commands/game_guide.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 require('dotenv').config();
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
@@ -19,11 +20,46 @@ module.exports = {
     const searchTerm = [gameValue, objectiveValue].filter(Boolean).join(' ');
 
     if (process.env.NODE_ENV == 'development') {
+      console.log('///search-term');
       console.dir(searchTerm, { depth: null });
-      console.log('////////////////////////////');
     }
 
+    wikiSearch(searchTerm)
+      .then(function(response) {
 
+        if (process.env.NODE_ENV == 'development') {
+          console.log('///response');
+          console.dir(response.data, { depth: null });
+        }
+
+        const searchResult = response.data.data[0];
+
+        const row = new ActionRowBuilder()
+          .addComponents(
+            new ButtonBuilder()
+              .setLabel('Open Guide')
+              .setStyle(ButtonStyle.Link)
+              .setURL(searchResult.url),
+          );
+
+        const embed = new EmbedBuilder()
+          .setColor(0x0f6fec)
+          .setTitle(searchResult.title)
+          .setURL(searchResult.url)
+          .setDescription(searchResult.url);
+
+        return interaction.reply({ content: `Result for: \`${[gameValue, objectiveValue].filter(Boolean).join(', ')}\``, ephemeral: true, embeds: [embed], components: [row] });
+      }).catch(function(error) {
+        if (process.env.NODE_ENV == 'development') {
+          console.log('///error');
+          console.dir(error, { depth: null });
+        }
+        return interaction.reply({ content: 'We had an issue finding your search :(', ephemeral: true });
+      });
 
   },
 };
+
+function wikiSearch(query) {
+  return axios.get(`${process.env.API_HOST}/api/v1/search/wiki`, { params: { q: query } });
+}
